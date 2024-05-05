c1601d0c31e0b4e01d56a32651efd510e352e1af
diegorbb <diegomairenna@gmail.com>
2024-05-05 19:38:36 +0100

    added user auth and tweaked a few templates

--- inventory/app/urls.py
+++ inventory/app/urls.py
@@ -3,6 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+
+    path('login/', views.loginPage, name='login'),
+    path('logout/', views.logoutUser, name='logout'),
+
     path('create-item/', views.createItem, name='create-item'),
     path('edit-item/<str:pk>/', views.editItem, name='edit-item'),
     path('delete-item/<str:pk>/', views.deleteItem, name='delete-item'),
--- inventory/app/views.py
+++ inventory/app/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from django.http import HttpResponse
 from .models import Item
 from .forms import ItemForm
 
+@login_required(login_url='login')
 def home(request):
 
     items = Item.objects.all()
@@ -14,6 +19,35 @@ def home(request):
     return render(request, 'app/home.html', context)
 
 
+def loginPage(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'Incorrect user or password, try again!')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'User does not exist.')
+    return render(request, 'app/login.html')
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
+
+
+@login_required(login_url='login')
 def createItem(request):
     form = ItemForm()
 
@@ -27,6 +61,7 @@ def createItem(request):
     return render(request, 'app/create_item.html', context)
 
 
+@login_required(login_url='login')
 def editItem(request, pk):
     item = Item.objects.get(id=pk)
     form = ItemForm(instance=item)
@@ -34,6 +69,7 @@ def editItem(request, pk):
     if request.method == 'POST':
         form = ItemForm(request.POST, instance=item)
         if form.is_valid():
+            form.instance.updated_by = request.user
             form.save()
             return redirect('home')
 
@@ -41,6 +77,7 @@ def editItem(request, pk):
     return render(request, 'app/edit_item.html', context)
 
 
+@login_required(login_url='login')
 def deleteItem(request, pk):
     item = Item.objects.get(id=pk)
 
